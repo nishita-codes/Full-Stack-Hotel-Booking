247d5e73aa0fa991d7b2e7196004af9e809d7de6
Nishita-codes <nishitapatidar03@gmail.com>
2025-11-17 17:11:09 +0530

    added backend

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ import clerkWebhooks from "./controller/clerkWebhooks.js";
 import hotelRouter from "./routes/hotelRoutes.js";
 import connectCloudinary from "./configs/cloudinary.js";
 import roomRouter from "./routes/roomRoutes.js";
+import bookingRouter from "./routes/bookingRoutes.js";
 
 connectDB();
 connectCloudinary();
@@ -25,6 +26,7 @@ app.get('/' ,(req , res)=> res.send("API is working "))
 app.use('/api/user' , userRouter);
 app.use('/api/hotels' , hotelRouter);
 app.use('/api/rooms' , roomRouter);
+app.use('/api/bookings' , bookingRouter);
 
 const PORT = process.env.PORT || 3000;
 
